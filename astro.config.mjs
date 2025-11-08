41550d0e327939b04dec024a6f4ebb9035dcd472
Sascha Khbabez <sascha.khbabez@outlook.de>
2025-11-08 16:25:20 +0100

    added button and card

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Josefin Sans",
+        cssVariable: "--font-josefin",
+      },
+    ],
+  },
 });
